892771836c17a2ca370e003e52c764cb400442b0
Hussam Aldeen <h-ssom1997@hotmail.com>
2020-10-06 16:32:35 +0200

    init

---
+++ assignment 2/main/assignment2/impl/MyArrayMathImpl.java
@@ -0,0 +1,25 @@
+package main.assignment2.impl;
+
+import main.assignment2.ArrayMath;
+
+public class MyArrayMathImpl implements ArrayMath {
+
+    @Override
+    public boolean isSameCollection(int[] array1, int[] array2) {
+	// TODO Auto-generated method stub
+	return false;
+    }
+
+    @Override
+    public int minDifferences(int[] array1, int[] array2) {
+	// TODO Auto-generated method stub
+	return 0;
+    }
+
+    @Override
+    public int[] getPercentileRange(int[] arr, int lower, int upper) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+}
